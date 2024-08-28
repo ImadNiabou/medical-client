14fb3bb83c5aaadff226f74e69d5069cde29890c
Imad Niabou <141964358+ImadNiabou@users.noreply.github.com>
2024-08-28 23:44:39 +0200

    Last commit

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "http://localhost:3000/api",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
